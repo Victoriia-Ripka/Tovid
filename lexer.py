0d54bcf81504c650457443031938cb25a451faff
Dimon Stelmakh <105310426+DimonStelmakh@users.noreply.github.com>
2023-10-24 23:42:21 +0300

    Files have been merged, PEP8 abidance has been implemented, lex() call has been added

--- lexer.py
+++ lexer.py
@@ -10,6 +10,7 @@ token_table = {'true': 'keyword', 'false': 'keyword', 'const': 'keyword', 'var':
                }
 token_state_table = {2: 'ident', 4: 'int', 7: 'float', 11: 'complex', 19: 'string', 16: 'rel_op', 17: 'rel_op'}
 
+
 def class_of_char(char):
     if char is '.':
         result = "dot"
@@ -30,14 +31,17 @@ def class_of_char(char):
 
 
 stf = {(0, 'Letter'): 1, (1, 'Letter'): 1, (1, 'Digit'): 1, (1, 'other'): 2,
-       (0, 'Digit'): 3, (3, 'Digit'): 3, (3, 'other'): 4, (3, 'dot'): 5, (5, 'Digit'): 6, (5, 'other'): 103, (6, 'Digit'): 6, (6, 'other'): 7, (6, '+'): 8, (8, 'Digit'): 8, (8, 'dot'): 9, (8, 'i'): 11, (9, 'Digit'): 10, (9, 'other'): 103, (10, 'Digit'): 10, (10, 'i'): 11,
+       (0, 'Digit'): 3, (3, 'Digit'): 3, (3, 'other'): 4, (3, 'dot'): 5, (5, 'Digit'): 6, (5, 'other'): 103,
+       (6, 'Digit'): 6, (6, 'other'): 7, (6, '+'): 8, (8, 'Digit'): 8, (8, 'dot'): 9, (8, 'i'): 11, (9, 'Digit'): 10,
+       (9, 'other'): 103, (10, 'Digit'): 10, (10, 'i'): 11,
        (0, ': '): 12, (12, '='): 13, (12, 'other'): 102,
        (0, 'other'): 101,
        (0, 'cr'): 14,
        (0, '='): 15, (15, '='): 16,
        (0, '<'): 17, (0, '>'): 17,
        (0, '\"'): 18, (18, 'other'): 18, (18, '\"'): 19,
-       (0, '{'): 20, (0, '}'): 20, (0, '^'): 20, (0, '/'): 20, (0, '*'): 20, (0, '-'): 20, (0, '+'): 20, (0, '('): 20, (0, ')'): 20
+       (0, '{'): 20, (0, '}'): 20, (0, '^'): 20, (0, '/'): 20, (0, '*'): 20, (0, '-'): 20, (0, '+'): 20,
+       (0, '('): 20, (0, ')'): 20
        }
 init_state = 0
 F = {2, 4, 7, 11, 13, 14, 16, 17, 19, 20, 101, 102, 103}
@@ -70,16 +74,16 @@ def is_final(state):
         return False
 
 
-def next_state(state,class_ch):
+def next_state(state, class_ch):
     try:
         return stf[(state, class_ch)]
     except KeyError:
-        return stf[(state,'other')]
+        return stf[(state, 'other')]
 
 
 def next_char():
     global num_char
-    num_char+=1
+    num_char += 1
     return source_code[num_char]
 
 
@@ -120,6 +124,34 @@ def lex():
         print('Lexer: Аварійне завершення програми з кодом {0}'.format(error))
 
 
+def processing():
+    global state, lexeme, char, num_line, num_char, table_of_symb
+    if state == 14:
+        num_line += 1
+        state = init_state
+    if state == 2:
+        token = get_token(state, lexeme)
+        if token != 'keyword':
+            index = index_id_const(state, lexeme)
+            print('{0:<3d} {1:<10s} {2:<10s} {3:<2d} '.format(num_line, lexeme, token, index))
+            table_of_symb[len(table_of_symb)+1] = (num_line, lexeme, token, index)
+        else:  # якщо keyword
+            print('{0:<3d} {1:<10s} {2:<10s} '.format(num_line, lexeme, token))
+            table_of_symb[len(table_of_symb)+1] = (num_line, lexeme, token, '')
+            lexeme = ''
+            num_char = put_char_back(num_char)  # зiрочка
+            state = init_state
+    if state in (4, 7, 11, 13, 14, 16, 17, 19, 20):
+        lexeme += char
+        token = get_token(state, lexeme)
+        print('{0:<3d} {1:<10s} {2:<10s} '.format(num_line, lexeme, token))
+        table_of_symb[len(table_of_symb)+1] = (num_line, lexeme, token, '')
+        lexeme = ''
+        state = init_state
+    if state in F_error:  # (101, 102, 103): # ERROR
+        fail()
+
+
 def get_token(state, lexeme):
     try:
         return token_table[lexeme]
@@ -127,7 +159,7 @@ def get_token(state, lexeme):
         return token_state_table[state]
 
 
-def index_id_const(state,lexeme):
+def index_id_const(state, lexeme):
     indx = 0
     if state == 2:
         indx = table_of_id.get(lexeme)
@@ -135,11 +167,14 @@ def index_id_const(state,lexeme):
             indx = len(table_of_id)+1
             table_of_id[lexeme] = indx
     if state in (4, 7, 11):
-        indx=table_of_const.get(lexeme)
+        indx = table_of_const.get(lexeme)
         if not indx:
-            indx=len(table_of_const)+1
+            indx = len(table_of_const)+1
             table_of_const[lexeme] = (token_state_table[state], indx)
     return indx
+
+
+lex()
 
 
 print('-' * 30)
