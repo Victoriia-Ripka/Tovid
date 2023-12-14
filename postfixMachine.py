de8c0e68a8e0bd4dbd8c8b056a3d0073135cdc12
Dimon Stelmakh <105310426+DimonStelmakh@users.noreply.github.com>
2023-12-14 17:17:15 +0200

    Input bug fixed

--- postfixMachine.py
+++ postfixMachine.py
@@ -173,15 +173,15 @@ class PSM:             # Postfix Stack Machine
                             print(f'-------------- OUT: {id}')
                 elif tok == 'in_op':
                         id, _ = self.stack.get_top_element()
-                        #user_input = '"' + input() + '"'
-                        #print(f'-------------- IN: {id}={self.tableOfVar[id][2]} -> {id}={user_input}')  # {id}={self.tableOfVar[id][2]} -> {id}={user_input}')
-                        #self.stack.push((user_input, 'string'))
-                        #self.do_it(':=', 'assign_op')
+                        user_input = '"' + input() + '"'
+                        print(f'-------------- IN: {id}={self.tableOfVar[id][2]} -> {id}={user_input}')  # {id}={self.tableOfVar[id][2]} -> {id}={user_input}')
+                        self.stack.push((user_input, 'string'))
+                        self.do_it(':=', 'assign_op')
                         self.numInstr += 1
                         # if id in self.tableOfVar.keys():
                         #     self.tableOfVar[lexL] = (self.tableOfVar[lexL][0], tokR, get_value(lexR, tokR))
                         # else:
-                        #     self.tableOfNamedConst[lexL][0], tokR, get_value(lexR, tokR))
+                        #     self.tableOfNamedConst[lexL][0], tokR, get_value(lexR, tokR)
                 else:
                     print(f'-=-=-=========({lex},{tok})  numInstr={self.numInstr}')
                     self.do_it(lex, tok)
